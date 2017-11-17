4a7d46a6b20a30ebd88767530d273b4da37d2b18
gdavila09 <grrd99@gmail.com>
2017-11-16 20:05:36 -0600

    remove second weekday

--- db/migrate/20171031034037_add_message_to_notification.rb
+++ db/migrate/20171031034037_add_message_to_notification.rb
@@ -4,6 +4,6 @@ class AddMessageToNotification < ActiveRecord::Migration[5.0]
     # remove_column :notifications, :weekday
     add_column :notifications, :message_sent , :integer
     add_column :badges, :dns, :string
-    add_column :notifications, :weekday , :integer
+    # add_column :notifications, :weekday , :integer
   end
 end
